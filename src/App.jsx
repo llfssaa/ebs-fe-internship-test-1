edebf896d0c7a9c777e4fbd456185f4d732a0d41
llfssaa <liubovitopolcean@gmail.com>
2022-11-17 13:13:36 +0200

    add husky

--- src/App.jsx
+++ src/App.jsx
@@ -3,31 +3,21 @@ import './App.css';
 
 function App() {
   return (
-         <div className="App">
+    <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-               Edit <code>
-
-          src/App.js</
-          code> and save to
-          reload.
+          Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="A
-          pp-link"
-          href="https:/
-          /reactjs.org"
+          className="App-link"
+          href="https://reactjs.org"
           target="_blank"
-          re
-          l="noopener noreferrer" rel="noreferrer"
+          rel="noopener noreferrer"
         >
           Learn React
         </a>
-      </header
-
-
-      >
+      </header>
     </div>
   );
 }
