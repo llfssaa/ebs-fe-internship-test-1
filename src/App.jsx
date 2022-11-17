2b674d024ef28e5b042dbb99482ab5bb5714df3a
llfssaa <liubovitopolcean@gmail.com>
2022-11-17 13:11:13 +0200

    test commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,31 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+         <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+               Edit <code>
+
+          src/App.js</
+          code> and save to
+          reload.
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          className="A
+          pp-link"
+          href="https:/
+          /reactjs.org"
           target="_blank"
-          rel="noopener noreferrer"
+          re
+          l="noopener noreferrer" rel="noreferrer"
         >
           Learn React
         </a>
-      </header>
+      </header
+
+
+      >
     </div>
   );
 }
